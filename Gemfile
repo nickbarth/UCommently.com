02d80bef5e05b098379138b05ba1fddc66e4d797
Nick Barth <nick@nickbarth.ca>
2012-10-13 19:47:23 +0000

    setup a database

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source :rubygems
 gem 'sinatra', require: 'sinatra/base'
 gem 'haml', require: 'haml'
 gem 'sprock-assets', require: 'sprock_assets'
+gem 'activerecord', require: 'active_record'
 gem 'thin'
--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,16 @@
 require 'bundler'
 Bundler.require
 
+require 'logger'
+
 class SinatraApp < Sinatra::Base
-  use SprockAssets
+  before { ActiveRecord::Base.verify_active_connections! }
+  after  { ActiveRecord::Base.clear_active_connections! }
+
+  configure :development do
+    use SprockAssets
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
 
   configure do
     set :root, File.expand_path(File.join(File.dirname(__FILE__), '../'))
