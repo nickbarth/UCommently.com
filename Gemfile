696995685223a8742890cd4d8d48ddac9b5e9568
Nick Barth <nick@nickbarth.ca>
2012-10-13 08:15:45 +0000

    setup assets

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source :rubygems
 
 gem 'sinatra', require: 'sinatra/base'
 gem 'haml', require: 'haml'
+gem 'sprock-assets', require: 'sprock_assets'
 gem 'thin'
--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,8 @@ require 'bundler'
 Bundler.require
 
 class SinatraApp < Sinatra::Base
+  use SprockAssets
+
   configure do
     set :root, File.expand_path(File.join(File.dirname(__FILE__), '../'))
     set :views, 'app/views'
